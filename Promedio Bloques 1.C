959d04d0889de6c614454436489fcf8ddcca04f3
Marcos-Giovannelli <116891079+Marcos-Giovannelli@users.noreply.github.com>
2022-11-13 19:16:12 -0300

    Create Promedio Bloques 1.C

---
+++ Promedio Bloques 1.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+float nota;
+
+int main(void)
+{
+  int cantidad = 0;
+  int n = 0;
+  float sum;
+  printf("Ingrese las notas para obetener el promedio y colocar -1 para obtener   el resultado \n \n");
+  for (n=1; n<=100; n++)
+    {
+      printf("Ingrese la nota %d\n", n);
+      scanf("%f", &nota);
+      if (nota != -1)
+        {
+        sum=(sum+nota);
+        cantidad++;
+        }
+      else
+        {  
+        printf("Cantidad de notas: %d\n", n-1);
+        break;
+        } 
+    }
+  float promedio = (sum/cantidad);
+  printf("El promedio es: %.f \n", promedio);
+  return 0;
+}
